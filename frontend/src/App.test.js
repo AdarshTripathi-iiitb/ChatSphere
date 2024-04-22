86b38d992844e134bcd044af4f7b0944d758d75e
Adarsh Tripathi <adarshphysics1973@gmail.com>
2024-04-22 23:10:50 +0530

    frontend test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders app with initial state', () => {
+  const { getByText, getByPlaceholderText } = render(<App />);
+  
+  expect(getByText('BChat A Multi Room Chat Application')).toBeInTheDocument();
+  expect(getByPlaceholderText('@Username')).toBeInTheDocument();
+  expect(getByPlaceholderText('#Room')).toBeInTheDocument();
+});
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Add more tests as needed for your application
